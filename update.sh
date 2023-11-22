189c7b5c4051f00931f73ace43328583963ff510
bvn-roothide <bvnunlock9999@gmail.com>
2023-11-22 21:53:41 +0700

    -

--- update.sh
+++ update.sh
@@ -55,7 +55,7 @@ apt-ftparchive packages ./debs > ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
 echo "------------------"
 echo "Building Release...."
-printf "Origin: BVN Roothide Repo \nLabel: Repo Roothide\nSuite: stable\nVersion: 1.1\nCodename: ios\nArchitecture: iphoneos-arm iphoneos-arm64\nComponents: main\nDescription: Apple-Support\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: BVN Roothide Repo \nLabel: iPhone/iPad-Jailbreak\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitectures: iphoneos-arm iphoneos-arm64\nComponents: main\nDescription: Apple/Support/Jailbreak\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 
 echo "------------------"
 echo "Done!"
